b3e48d2355caf220ced7b43ffdbf47890b469bca
Thien L <thien.luu@students.mq.edu.au>
2019-08-03 23:41:45 +1000

    Add project files.

---
+++ Resume_WebApp/wwwroot/JS/Clock.js
@@ -0,0 +1,34 @@
+﻿function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var don = dayOrNight(h);
+
+    m = checkTime(m);
+    s = checkTime(s);
+    h = notTwentyFour(h);
+
+    document.getElementById('theTime').innerHTML =
+        h + ":" + m + ":" + s + " " + don;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+
+function notTwentyFour(h) {
+    if (h > 12) { h = "0" + (h - 12) };
+    return h;
+}
+
+function dayOrNight(i) {
+    if (i > 12) {
+        i = "PM"
+    }
+    else {
+        i = "AM"
+    }
+    return i;
+}
---
+++ Resume_WebApp/wwwroot/JS/SideNav.js
@@ -0,0 +1,34 @@
+﻿//$(window).resize(function () {
+//    if ($(window).width() > 576) {
+//        window.onscroll = function () {
+//            var currentScrollPos = window.pageYOffset;
+//            if (currentScrollPos < 70) {
+//                document.getElementById("sideNav").style.display = "none";
+//            } else {
+//                document.getElementById("sideNav").style.display = "inline";
+//            }
+//        }
+//    }
+//    else {
+//        document.getElementById("sideNav").style.display = "none";
+//    }
+//});
+
+$(window).resize(function () {
+    if ($(window).width() > 576) {
+        window.onscroll = function () {
+            var currentScrollPos = window.pageYOffset;
+            if (currentScrollPos < 70) {
+                document.getElementById("sideNav").style.display = "none";
+            } else {
+                document.getElementById("sideNav").style.display = "inline";
+            }
+        }
+    }
+    else {
+        document.getElementById("sideNav").style.display = "none";
+    }
+});
+$(document).ready(function () {
+    $(window).trigger('resize');
+});
